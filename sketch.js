9fbf9317e688342e481debcf820e85f03b86c5c6
trish339 <69069835+trish339@users.noreply.github.com>
2020-08-19 20:39:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.scale=0.6
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:false});
 	World.add(world, packageBody);
 	
 
